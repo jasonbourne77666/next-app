3527ce8ca412e84826c11dab9c1d325fefc16ad5
Jason <jason@gmail.com>
2023-10-18 16:36:32 +0800

    feat: 表单封装 useForm

---
+++ src/app/practice/form/HooksForm/form.tsx
@@ -0,0 +1,65 @@
+import { forwardRef, useImperativeHandle } from 'react';
+import FormContext from './useForm/FormContext';
+import { DataProps, FormInstance } from './useForm/interface.d';
+import useForm from './useForm';
+
+interface FormProps {
+  onReset?: () => void;
+  onFinish?: (data: any) => void;
+  onFinishFailed?: (errorInfo: any) => void;
+  initialValues?: DataProps;
+  form?: FormInstance;
+  [key: string]: any;
+}
+
+const Index = (props: FormProps, ref: any) => {
+  const {
+    form,
+    children,
+    onFinish = (data: any) => {},
+    onReset = () => {},
+    onFinishFailed = (errorInfo: any) => {},
+    initialValues = {},
+    ...payload
+  } = props;
+
+  const [formRef] = useForm(initialValues, form);
+
+  // 用于剔除方法，不提供给外部使用
+  const {
+    registerField,
+    unRegisterField,
+    dispatch,
+    setConfigWays,
+    ...formRefInstance // 需要暴露的方法
+  } = formRef;
+
+  /* Form 能够被 ref 标记，并操作实例。 */
+  useImperativeHandle(ref, () => formRefInstance, [formRefInstance]);
+
+  formRef.setConfigWays({
+    onFinish,
+    onReset,
+    onFinishFailed,
+  });
+
+  return (
+    <form
+      {...payload}
+      onSubmit={(e) => {
+        // 阻止默认事件
+        e.preventDefault();
+        e.stopPropagation();
+        formRef.submit();
+      }}
+      onReset={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        formRef.resetFields(); /* 重置表单 */
+      }}>
+      <FormContext.Provider value={formRef}>{children}</FormContext.Provider>
+    </form>
+  );
+};
+
+export default forwardRef(Index);
---
+++ src/app/practice/form/HooksForm/formItem.tsx
@@ -0,0 +1,98 @@
+import {
+  cloneElement,
+  isValidElement,
+  useEffect,
+  useContext,
+  useReducer,
+  useMemo,
+} from 'react';
+import { Layout } from './FormItemCommon';
+import FormContext from './useForm/FormContext';
+import { FormInstance, NameProps, updateProps } from './useForm/interface.d';
+
+// onFinishFailed 回调后的函数
+
+interface FormItemProps {
+  label?: string;
+  tooltip?: string;
+  [key: string]: any;
+}
+
+const FormItem = (props: FormItemProps) => {
+  const [, update] = useReducer((num: number): number => num + 1, 0);
+  const { name, children } = props;
+
+  // const [state, dispatch] = useConnect();
+  const contextValue = useContext(FormContext);
+
+  const {
+    getFieldValue,
+    dispatch,
+    registerField,
+    unRegisterField,
+    getFieldValidate,
+  } = contextValue;
+
+  // 优化
+  const updateChange: updateProps = useMemo(() => {
+    return {
+      message: props?.message || `请填写${props?.label}字段`,
+      required: props?.required,
+      rules: props?.rules,
+      updateValue: () => update(),
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [contextValue, name]);
+
+  useEffect(() => {
+    // 注册
+    name && registerField(name, updateChange);
+    return () => {
+      //卸载
+      name && unRegisterField(name);
+    };
+  }, [updateChange, name, registerField, unRegisterField]);
+
+  let childrenPro;
+
+  // 利用 isValidElement 来判断传递的数据是否是 React.ReactElement. 注意他可以判断多节点的情况，和无值的情况
+  if (isValidElement(children) && name) {
+    // 利用 cloneElement 给传递的组件加入 value 和 onChange 属性，剥离出对应的方法
+    childrenPro = cloneElement(children as React.ReactElement, {
+      value: getFieldValue(name),
+      onChange: (v: any) => {
+        const payload: any = {};
+
+        // 判断属于那种
+        const value = v?.target?.localName === 'input' ? v?.target?.value : v;
+        payload[name] = value;
+
+        // 更新 store 中的值
+        dispatch({
+          type: 'updateValue',
+          name,
+          value,
+        });
+
+        // 触发验证
+        dispatch({
+          type: 'validateField',
+          name,
+        });
+      },
+      status: getFieldValidate(name)?.status === 'rej' ? 'error' : undefined,
+    });
+  } else {
+    childrenPro = children;
+  }
+  if (name === 'rules') {
+    console.log(props);
+  }
+  return (
+    <Layout {...props} {...getFieldValidate(name)}>
+      {childrenPro}
+    </Layout>
+  );
+};
+
+export default FormItem;
